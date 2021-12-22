d88362bd1c2ff073377708dea232447a9efa0d8e
DIVYA SHARMA <divya@racloop.com>
2021-12-22 17:03:37 +0530

    Send & Recieve Message

---
+++ src/main/java/com/sqsapplication/SQSSetup/Queue3.java
@@ -0,0 +1,65 @@
+package com.sqsapplication.SQSSetup;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.AmazonSQSException;
+import com.amazonaws.services.sqs.model.CreateQueueResult;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import java.util.Date;
+import java.util.List;
+public class Queue3 {
+
+    private static final String QUEUE_NAME = "Queue3" + new Date().getTime();
+
+
+    public static void main(String[] args)
+    {
+        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
+        try {
+            CreateQueueResult create_result = sqs.createQueue(QUEUE_NAME);
+        } catch (AmazonSQSException e) {
+            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+                throw e;
+            }
+        }
+
+        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+
+        SendMessageRequest send_msg_request = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageBody("Queue 3")
+                .withDelaySeconds(5);
+        sqs.sendMessage(send_msg_request);
+
+
+        // Send multiple messages to the queue
+        SendMessageBatchRequest send_batch_request = new SendMessageBatchRequest()
+                .withQueueUrl(queueUrl)
+                .withEntries(
+                        new SendMessageBatchRequestEntry(
+                                "msg_1", "Hello from queue 3 message 1"),
+                        new SendMessageBatchRequestEntry(
+                                "msg_2", "Hello from queue 3 message 2"),
+                        new SendMessageBatchRequestEntry(
+                                "msg_3", "Hello from queue 3 message 3")
+                                .withDelaySeconds(10),
+                        new SendMessageBatchRequestEntry(
+                                "msg_4", "Hello from queue 3 message 4"),
+                        new SendMessageBatchRequestEntry(
+                                "msg_5", "Hello from queue 3 message 5")
+                                .withDelaySeconds(10));
+        sqs.sendMessageBatch(send_batch_request);
+
+        // receive messages from the queue
+        List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
+
+        // delete messages from the queue
+        for (Message m : messages) {
+            sqs.deleteMessage(queueUrl, m.getReceiptHandle());
+        }
+    }
+}
---
+++ src/test/java/com/sqsapplication/SqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
